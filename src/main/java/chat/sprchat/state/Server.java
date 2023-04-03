591de32e9915d8de0acd0fdf683bc9aff2ff3cb9
Virga Balint <sidosi70@gmail.com>
2023-04-03 20:07:56 +0200

    1st commit, added states(server, client)

--- src/main/java/chat/sprchat/state/Server.java
+++ src/main/java/chat/sprchat/state/Server.java
@@ -1,6 +1,49 @@
 package chat.sprchat.state;
 
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import org.springframework.stereotype.Component;
+
+import java.net.InetSocketAddress;
+
+@Component
 public class Server extends WebSocketServer
 {
-    
+    public Server(InetSocket address)
+    {
+        super(address);
+        this.start();
+        System.out.println(InetSocket.ip);
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake)
+    {
+
+    }
+
+    @Override
+    public void onClose(WebSocket webSocket, int i, String s, boolean b)
+    {
+
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, String s)
+    {
+
+    }
+
+    @Override
+    public void onError(WebSocket webSocket, Exception e)
+    {
+
+    }
+
+    @Override
+    public void onStart()
+    {
+
+    }
 }
